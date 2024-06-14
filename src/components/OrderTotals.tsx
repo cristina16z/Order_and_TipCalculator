e9f57c82fa8ee189ee2faa979fb5515f30d629d8
cristina16z <2023_cristina.val@iticbcn.cat>
2024-06-14 10:09:41 +0200

    TotalAmount

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -11,6 +11,7 @@ export default function OrderTotals({order, tip}: OrderTotalsProps) {
  
     const subtotalAmount = useMemo(() => order.reduce((total,item) => total + (item.quantity * item.price),0), [order])
     const tipAmount = useMemo(() => subtotalAmount*tip, [tip, order])
+    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [tip, order])
 
  
 
@@ -32,7 +33,7 @@ export default function OrderTotals({order, tip}: OrderTotalsProps) {
 
 
                 <p>Total a pagar: {' '}
-                    <span className="font-bold">$0</span>
+                    <span className="font-bold">{formatCurrency(totalAmount)}</span>
                 </p>
 
             </div>
